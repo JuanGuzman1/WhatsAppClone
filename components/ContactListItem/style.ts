6eecb65af3ff180cb3dc4ae54850792da0d89b8a
Juan Guzman <juanltd07@gmail.com>
2021-03-22 21:19:40 -0600

    chat room and input box

---
+++ components/ContactListItem/style.ts
@@ -0,0 +1,32 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        padding: 15
+    },
+    leftContainer: {
+        flexDirection: 'row'
+    },
+    avatar: {
+        width: 60,
+        height: 60,
+        marginRight: 10,
+        borderRadius: 50
+    },
+    username: {
+        fontWeight: 'bold',
+        fontSize: 16
+    },
+    status: {
+        fontSize: 16,
+        color: 'grey'
+    },
+    midContainer: {
+        justifyContent: 'space-around'
+    }
+});
+
+export default styles;
---
+++ components/InputBox/index.tsx
@@ -0,0 +1,73 @@
+import {
+  MaterialCommunityIcons,
+  FontAwesome5,
+  Entypo,
+  Fontisto,
+  MaterialIcons,
+} from "@expo/vector-icons";
+import React, { useState } from "react";
+import { View, TextInput, Text, TouchableOpacity } from "react-native";
+import styles from "./styles";
+
+const InputBox = () => {
+  const [message, setMessage] = useState();
+  const onMicrophonePress = ()=> {
+      console.warn('microphone');
+  }
+  const onSendPress = ()=> {
+    console.warn('send');
+    //send the message to the backend
+
+    setMessage('');
+}
+  const onPress = () => {
+    if (!message) {
+      onMicrophonePress();
+    } else {
+      onSendPress();
+    }
+  };
+  return (
+    <View style={styles.container}>
+      <View style={styles.mainContainer}>
+        <FontAwesome5 name="laugh-beam" size={24} color={"grey"} />
+        <TextInput
+          placeholder={"Type a message"}
+          style={styles.textInput}
+          multiline
+          value={message}
+          onChangeText={setMessage}
+        />
+        <Entypo
+          name="attachment"
+          size={24}
+          color={"grey"}
+          style={styles.icon}
+        />
+        {!message && (
+          <Fontisto
+            name="camera"
+            size={24}
+            color={"grey"}
+            style={styles.icon}
+          />
+        )}
+      </View>
+      <TouchableOpacity onPress={onPress}>
+        <View style={styles.buttonContainer}>
+          {!message ? (
+            <MaterialCommunityIcons
+              name="microphone"
+              size={28}
+              color={"white"}
+            />
+          ) : (
+            <MaterialIcons name="send" size={28} color={"white"} />
+          )}
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default InputBox;
