289583211ffaf2d269bde8831abb95a304ce6636
Juan Guzman <juanltd07@gmail.com>
2021-03-23 22:57:13 -0600

    chat rooms displayed

--- screens/ChatsScreen.tsx
+++ screens/ChatsScreen.tsx
@@ -1,18 +1,38 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, FlatList } from "react-native";
 
 import ChatListItem from "../components/ChatListItem";
 import NewMessageButton from "../components/NewMessageButton";
 import { Text, View } from "../components/Themed";
-import ChatRooms from "../data/ChatRooms";
+import {API,graphqlOperation,Auth} from 'aws-amplify';
+import {getUser} from './queries';
 
 export default function ChatsScreen() {
+const [chatRooms,setChatRooms] = useState([]);
+
+  useEffect(() => {
+    const fetchChatRooms = async () => {
+      try{
+        const userInfo = await Auth.currentAuthenticatedUser();
+        const userData = await API.graphql(graphqlOperation(
+          getUser,{
+            id: userInfo.attributes.sub
+          }
+        ))
+        setChatRooms(userData.data.getUser.chatRoomUser.items);
+      }catch(e){
+        console.log(e);
+      }
+    };
+    fetchChatRooms();
+  }, []);
+
   return (
     <View style={styles.container}>
       <FlatList
         style={{ width: "100%" }}
-        data={ChatRooms}
-        renderItem={({ item }) => <ChatListItem chatRoom={item} />}
+        data={chatRooms}
+        renderItem={({ item }) => <ChatListItem chatRoom={item.chatRoom} />}
         keyExtractor={(item) => item.id}
       />
       <NewMessageButton />
