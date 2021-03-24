07449db2f5b0a0e3a23ce1f8ff14e602342ee43d
Juan Guzman <juanltd07@gmail.com>
2021-03-23 21:22:38 -0600

    create chat room

--- components/ContactListItem/index.tsx
+++ components/ContactListItem/index.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import { View, Text, Image, TouchableWithoutFeedback } from "react-native";
 import { User } from "../../types";
 import styles from "./style";
-import moment from "moment";
+import { API, graphqlOperation, Auth } from "aws-amplify";
+import {
+  createChatRoom,
+  createChatRoomUser,
+} from "../../src/graphql/mutations";
 import { useNavigation } from "@react-navigation/native";
 
 export type ContactListItemProps = {
@@ -12,8 +16,48 @@ export type ContactListItemProps = {
 const ContactListItem = (props: ContactListItemProps) => {
   const { user } = props;
   const navigation = useNavigation();
-  const onClick = () => {
-    //navigate to chatroom to these user
+
+  const onClick = async () => {
+    try {
+      //create a new chat room
+      const newChatData = await API.graphql(
+        graphqlOperation(createChatRoom, {
+          input: {},
+        })
+      );
+
+      if (!newChatData.data) {
+        console.log("failed to create a chat room");
+        return;
+      }
+      const newChatRoom = newChatData.data.createChatRoom;
+      //add user to the chat room
+      await API.graphql(
+        graphqlOperation(createChatRoomUser, {
+          input: {
+            userID: user.id,
+            chatRoomID: newChatRoom.id,
+          },
+        })
+      );
+      //add authenticated user to the chat room
+      const userInfo = await Auth.currentAuthenticatedUser();
+      await API.graphql(
+        graphqlOperation(createChatRoomUser, {
+          input: {
+            userID: userInfo.attributes.sub,
+            chatRoomID: newChatRoom.id,
+          },
+        })
+      );
+
+      navigation.navigate('ChatRoom',{
+        id: newChatRoom.id,
+        name: "harcoded name"
+      })
+    } catch (e) {
+      console.log(e);
+    }
   };
   return (
     <TouchableWithoutFeedback onPress={onClick}>
@@ -23,7 +67,9 @@ const ContactListItem = (props: ContactListItemProps) => {
 
           <View style={styles.midContainer}>
             <Text style={styles.username}>{user.name}</Text>
-            <Text numberOfLines={2} style={styles.status}>{user.status}</Text>
+            <Text numberOfLines={2} style={styles.status}>
+              {user.status}
+            </Text>
           </View>
         </View>
       </View>
--- screens/ContactsScreen.tsx
+++ screens/ContactsScreen.tsx
@@ -1,17 +1,34 @@
-import * as React from "react";
+import {API,  graphqlOperation } from "aws-amplify";
+import React, {useEffect, useState} from "react";
 import { StyleSheet, FlatList } from "react-native";
 
 import ContactListItem from "../components/ContactListItem";
 import NewMessageButton from "../components/NewMessageButton";
 import { Text, View } from "../components/Themed";
-import Users from "../data/Users";
+import { listUsers } from "../src/graphql/queries";
 
 export default function ContactsScreen() {
+  const [users,setUsers] = useState([]);
+
+  useEffect(()=>{
+    const fetchUsers = async()=>{
+      try {
+        const usersData = await API.graphql(graphqlOperation(
+          listUsers
+        ))
+        setUsers(usersData.data.listUsers.items);
+      }catch(e){
+        console.log(e);
+      }
+    }
+    fetchUsers();
+  },[])
+
   return (
     <View style={styles.container}>
       <FlatList
         style={{ width: "100%" }}
-        data={Users}
+        data={users}
         renderItem={({ item }) => <ContactListItem user={item} />}
         keyExtractor={(item) => item.id}
       />
